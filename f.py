b3aa3bf8cbe81c3f9702b1b4d8f240cb9b5afde6
Adam Thomas <adamthomas-001@hotmail.co.uk>
2016-01-21 15:40:36 +0000

    added password generation

--- f.py
+++ f.py
@@ -20,5 +20,17 @@ def get_fortune():
 	l = randint(0,len(fortunes["quotes"])-1)	
  	return Response(json.dumps(fortunes["quotes"][l],indent=3), mimetype='application/json')
 
+@app.route('/password/random', methods=['GET'])
+def get_password():
+	import random
+	i = 1
+	randPassword = ""
+
+	for i in range(0,12,1):
+	    charValue = random.randint(1,26)
+	    randChar = chr(96+int(charValue))
+	    randPassword = randPassword + randChar
+	return Response(randPassword)
+
 if __name__ == '__main__':
     app.run(debug=True)
