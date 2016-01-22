522ea06b307d157c4eb0ee1915e438482f4ed11a
Adam Thomas <adamthomas-001@hotmail.co.uk>
2016-01-22 15:58:30 +0000

    .

--- f.py
+++ f.py
@@ -17,10 +17,9 @@ def get_limerick():
 
 @app.route("/limerick/id/<int:id>")
 def get_fortune_by_id(id):
-        for i in range(0,len):
-		if fortunes["quotes"][c]["id"] == id:
-			l = i(limericks["quotes"])
-		        return Response(json.dumps(fortunes["quotes"][l],indent=3), mimetype='application/json')
+        for i in range(0,len(fortunes["quotes"])-1):
+		if fortunes["quotes"][i]["id"] == id:
+		        return Response(json.dumps(fortunes["quotes"][i],indent=3), mimetype='application/json')
 			break
 
 @app.route('/fortune/random', methods=['GET'])
